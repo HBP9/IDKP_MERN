7f72e0041ffe999f6b622b6cb110750a2a896e88
HBP9 <yashvj97@gmail.com>
2023-06-03 18:22:15 +0530

    home page added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home-container">
+        <div className="card-center">
+          <img src="/images/qr.png" alt="qr" className="qr-wall" />
+          <Link to={"/qr-scan"}>
+            <button type="button" className="btn btn-info">
+              Scan QR
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
