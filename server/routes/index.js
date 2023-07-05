a9730e2ee00351601846c7f77314c3f18037eb9c
HBP9 <yashvj97@gmail.com>
2023-07-05 19:32:11 +0530

    superadmin added

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,28 @@ const data = require("../adminList.js");
 router.use("/user", require("./user.js"));
 router.use("/admin", require("./admin.js"));
 
-router.post("/seed", async (req, res) => {
+// router.post("/seed", async (req, res) => {
+//   try {
+//     await Admins.deleteMany({});
+//     const newAdmins = await Admins.insertMany(data.Admins);
+//     return res.status(201).send({ newAdmins });
+//   } catch (error) {
+//     return res.status(500).send(error.message);
+//   }
+// });
+
+router.post("/superadmin", async (req, res) => {
+  const { username, password } = req.body;
+
   try {
-    await Admins.deleteMany({});
-    const newAdmins = await Admins.insertMany(data.Admins);
-    return res.status(201).send({ newAdmins });
+    const existingUser = await Admins.findOne({ username });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    const newAdmin = new Admins({ username, password });
+    await newAdmin.save();
+    return res.status(201).send("Admin Added");
   } catch (error) {
     return res.status(500).send(error.message);
   }
